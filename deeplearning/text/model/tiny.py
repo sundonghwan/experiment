2dfced734665ac591c32d07af68af88b0c62880a
sundonghwan <sundonghwan@sundonghwanui-Macmini.local>
2026-02-01 23:47:53 +0900

    init project

---
+++ deeplearning/text/model/tiny.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+import torch
+
+class TransformerBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+class TinyTransformer(nn.Module):
+    def __init__(self):
+        pass
+
